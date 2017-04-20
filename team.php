d4c903abda3e8c874ed1f3784b74c4668ab6033f
lucatunes <lucagiangalassi@gmail.com>
2017-04-20 12:20:49 -0500

    Update team.php

--- team.php
+++ team.php
@@ -10,6 +10,6 @@
         </style>
     </head>
     <body>
-            <iframe width="100%" height="100%" src="http://www.cnn.com" />
+            <iframe width="100%" height="100%" src="https://docs.google.com/document/d/1K8YYw32TdEN9qd_tkypKQXDxwMh8uM3iAnUi5BcQ6jU/pub?embedded=true" />
     </body>
 </html>
