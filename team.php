d92cda70e4d213d3113966ffe7e16d5d8f42725f
lucatunes <lucagiangalassi@gmail.com>
2017-04-20 12:08:57 -0500

    Update team.php

--- team.php
+++ team.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <body>
-
-<iframe src="http://thunkable.com"></iframe>
+  <iframe src="https://docs.google.com/document/d/1K8YYw32TdEN9qd_tkypKQXDxwMh8uM3iAnUi5BcQ6jU/pub?embedded=true"></iframe>
 </body>
 </html>
