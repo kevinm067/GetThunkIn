d9069c72bc0331e0501476793e703fc20fc029be
lucatunes <lucagiangalassi@gmail.com>
2017-04-20 12:48:33 -0500

    Update team.php

--- team.php
+++ team.php
@@ -1,43 +1,39 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
-<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-</head>
-<body>
-
-<div data-role="page" id="pageone">
-  <div data-role="header">
-    <h1>MiniLesson Directory. Click To Expand A MiniLesson</h1>
-  </div>
-
-  <div data-role="main" class="ui-content">
-    <div data-role="collapsible">
-      <h1>MiniLesson #1 - Creating A Project</h1>
-     
-
-    <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+          <title>Test Layout</title>
         <style type="text/css">
             body, html
             {
                 margin: 0; padding: 0; height: 100%; overflow: hidden;
             }
-        </style>
-    </head>
-    <body>
-            <iframe width="100%" height="100%" src="https://docs.google.com/document/d/1K8YYw32TdEN9qd_tkypKQXDxwMh8uM3iAnUi5BcQ6jU/pub?embedded=true" />
-    </body>
+</head>
+<body>
 
-    </div>
+<div class="container">
+  <h2>Expand and Collapse with different icons</h2>
+    <button type="button" class="btn btn-success" data-toggle="collapse" data-target="#demo">
+      <span class="glyphicon glyphicon-collapse-down"></span> Open
+    </button>
+  <div id="demo" class="collapse">
+                <iframe width="100%" height="100%" src="http://www.cnn.com" />
   </div>
+</div>
 
-  <div data-role="footer">
-    <h1>More Being Added Weekly!</h1>
-  </div>
-</div> 
+<script>
+$(document).ready(function(){
+  $("#demo").on("hide.bs.collapse", function(){
+    $(".btn").html('<span class="glyphicon glyphicon-collapse-down"></span> Open');
+  });
+  $("#demo").on("show.bs.collapse", function(){
+    $(".btn").html('<span class="glyphicon glyphicon-collapse-up"></span> Close');
+  });
+});
+</script>
 
 </body>
 </html>
-
