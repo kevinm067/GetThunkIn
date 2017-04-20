a39daf07e4bfae0d02cf7373afd1bcdbc7bb10d1
lucatunes <lucagiangalassi@gmail.com>
2017-04-20 12:17:41 -0500

    Update team.php

--- team.php
+++ team.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
-<html>
- <body>
-            <iframe width="100%" height="100%" src="https://docs.google.com/document/d/1K8YYw32TdEN9qd_tkypKQXDxwMh8uM3iAnUi5BcQ6jU/pub?embedded=true"/>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Test Layout</title>
+        <style type="text/css">
+            body, html
+            {
+                margin: 0; padding: 0; height: 100%; overflow: hidden;
+            }
+        </style>
+    </head>
+    <body>
+            <iframe width="100%" height="100%" src="http://www.cnn.com" />
     </body>
 </html>
