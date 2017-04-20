1824a27995c7560d2551966add93060a8b87ec62
lucatunes <lucagiangalassi@gmail.com>
2017-04-20 12:12:51 -0500

    Update team.php

--- team.php
+++ team.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html>
 <body>
+<div class="first-row">
+  <p>Some text</p>
+  <p>And some more text</p>
+</div>
+<div class="second-row">
   <iframe src="https://docs.google.com/document/d/1K8YYw32TdEN9qd_tkypKQXDxwMh8uM3iAnUi5BcQ6jU/pub?embedded=true"></iframe>
+</div>
 </body>
 </html>
