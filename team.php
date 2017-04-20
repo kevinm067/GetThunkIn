544dfadd767dcfa47b82a894d4fa5b9e469ef682
lucatunes <lucagiangalassi@gmail.com>
2017-04-20 12:27:09 -0500

    Update team.php

--- team.php
+++ team.php
@@ -1,29 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
-<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-</head>
-<body>
-
-<div data-role="page" id="pageone">
-  <div data-role="header">
-    <h1>Collapsible Blocks</h1>
-  </div>
-
-  <div data-role="main" class="ui-content">
-    <div data-role="collapsible">
-      <h1>Click me - I'm collapsible!</h1>
-      <p>I'm the expanded content.</p>
-    </div>
-  </div>
-
-  <div data-role="footer">
-    <h1>Insert Footer Text Here</h1>
-  </div>
-</div> 
-
-</body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Test Layout</title>
+        <style type="text/css">
+            body, html
+            {
+                margin: 0; padding: 0; height: 100%; overflow: hidden;
+            }
+        </style>
+    </head>
+    <body>
+            <iframe width="100%" height="100%" src="http://www.cnn.com" />
+    </body>
 </html>
