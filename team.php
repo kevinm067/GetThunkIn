2b53702fa55a949224fd4d277a9c4ab8e5bd4da8
lucatunes <lucagiangalassi@gmail.com>
2017-04-20 12:23:58 -0500

    Update team.php

--- team.php
+++ team.php
@@ -1,6 +1,22 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+</head>
+<body>
+
+<div data-role="page" id="pageone">
+  <div data-role="header">
+    <h1>Collapsible Blocks</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <div data-role="collapsible">
+      <h1>Click me - I'm collapsible!</h1>
+      <head>
         <title>Test Layout</title>
         <style type="text/css">
             body, html
@@ -10,6 +26,16 @@
         </style>
     </head>
     <body>
-            <iframe width="100%" height="100%" src="https://docs.google.com/document/d/1K8YYw32TdEN9qd_tkypKQXDxwMh8uM3iAnUi5BcQ6jU/pub?embedded=true" />
+            <iframe width="100%" height="100%" src="http://www.cnn.com" />
     </body>
+    </div>
+  </div>
+
+  <div data-role="footer">
+    <h1>Insert Footer Text Here</h1>
+  </div>
+</div> 
+
+</body>
 </html>
+
