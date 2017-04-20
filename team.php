837394bc7d18699ada9a0b82bb33923d8ae53b2e
lucatunes <lucagiangalassi@gmail.com>
2017-04-20 12:15:33 -0500

    Update team.php

--- team.php
+++ team.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
  <body>
-            <iframe width="100%" height="100%" src="https://docs.google.com/document/d/1K8YYw32TdEN9qd_tkypKQXDxwMh8uM3iAnUi5BcQ6jU/pub?embedded=true" />
+            <iframe width="100%" height="100%" src="https://docs.google.com/document/d/1K8YYw32TdEN9qd_tkypKQXDxwMh8uM3iAnUi5BcQ6jU/pub?embedded=true"/>
     </body>
 </html>
