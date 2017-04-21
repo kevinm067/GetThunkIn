0aa01dc55180ce7244b7bae1ff97d5ece48d6748
Sander Jochems <sanderjochems@hotmail.nl>
2017-04-21 11:58:57 +0200

    Create home.php
    DONT EDIT THIS!

---
+++ home.php
@@ -0,0 +1,153 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+	<title>Home | GetThunkin</title>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta http-equiv="Cache-control" content="max-age=3600">
+    <meta id="viewport" name="viewport" content="width=device-width, initial-scale=1">
+    <meta id="description" name="description" content="Thunkable is a drag and drop app creator based off of MIT AppInventor but with more components and an amazing community. This course was intended for use with Thunkable, but may also work on...">
+    <meta id="author" name="author" content="Sander Jochems">
+	<meta id="keywords" name="keywords" content="GetThunkIn">
+	<meta id="copyright" name='copyright' content="Sander Jochems">
+	<meta id="robots" name="robots" content="all">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+	<link href="/css/google-font/lato.css" rel="stylesheet" type="text/css">
+    <link href="/css/google-font/open-sans.css" rel="stylesheet" type="text/css">
+	
+    <link href="/css/bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="/css/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="/css/custom/custom.css" rel="stylesheet" type="text/css">
+	
+	<script src="/css/ajax/jquery.min.js" type="text/javascript"></script>
+	<script src="/css/bootstrap/bootstrap.min.js" type="text/javascript"></script>
+  </head>
+	
+<div id="header">
+
+<nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="197" style="border-radius: 0 !important;">
+  <div class="container">
+	<div class="navbar-header">
+	  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	  </button>
+	  <a class="navbar-brand accent" href="#">GetThunkIn</a>
+	</div>
+	<div class="navbar-collapse collapse">
+	  <ul class="nav navbar-nav">
+		<li class="active"><a href="/home"><i class="fa fa-home"></i> Home</a>
+		</li>
+		<li><a href="/team"><i class="fa fa-list"></i> Team</a>
+		</li>
+	  </ul>
+	</div>
+  </div>
+</nav>
+
+  <div style="background-image: url(../images/background.png); height: 250px">
+	<div class="container">
+	  <div class="banner">
+		<div class="wsite-section-elements">
+		  <div class="wsite-spacer" style="height:50px;"></div>
+		  <div class="wsite-spacer" style="height:50px;"></div>
+		  <h2 style="text-align:center;"><font color="#ffffff"><font size="7">Welcome to GetThunkin!</font></h2>
+		  <div class="wsite-spacer" style="height:50px;"></div>
+		  <div class="wsite-spacer" style="height:50px;"></div>
+		  <div class="wsite-spacer" style="height:50px;"></div>
+		</div>
+	  </div>
+	</div>
+  </div>
+
+  <div class="main-wrap">
+	<div id="wsite-content" class="wsite-elements wsite-not-footer">
+	
+	  <div class="wsite-section-wrap">
+		<div class="wsite-section wsite-body-section wsite-background-1" style="height: 448px;">
+		  <div class="wsite-section-content">
+			<div class="container">
+			  <div class="wsite-section-elements">
+				<div class="wsite-spacer" style="height:50px;"></div>
+				<h2 class="wsite-content-title" style="text-align:center;"><font color="#2a2a2a"><font size="7">What is Thunkable?</font></font></h2>
+				<div>
+				  <div class="wsite-multicol">
+					<div class="wsite-multicol-table-wrap" style="margin:0 -15px;">
+					  <table class="wsite-multicol-table">
+						<tbody class="wsite-multicol-tbody">
+						  <tr class="wsite-multicol-tr">
+							<td class="wsite-multicol-col" style="width:19.604852906208%; padding:0 15px;">
+							  <div class="wsite-spacer" style="height:50px;"></div>
+							</td>
+							<td class="wsite-multicol-col" style="width:60.942217173562%; padding:0 15px;">
+							  <div class="paragraph" style="text-align:center;"><font color="#2a2a2a">Thunkable is a drag and drop app creator based off of MIT AppInventor but with more components and an amazing community. This course was intended for use with Thunkable, but may also work on AppInventor. </font>&#8203;</div>
+						  	</td>
+							<td class="wsite-multicol-col" style="width:19.45292992023%; padding:0 15px;">
+							  <div class="wsite-spacer" style="height:50px;"></div>
+							</td>
+						  </tr>
+						</tbody>
+					  </table>
+					</div>
+				  </div>
+				</div>
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </div>
+	  
+	  <div class="wsite-section-wrap">
+		<div class="wsite-section wsite-body-section wsite-background-9">
+		  <div class="wsite-section-content">
+			<div class="container">
+			  <div class="wsite-section-elements">
+				<h2 class="wsite-content-title" style="text-align:center;"><font size="6">Lessons 1 :&nbsp;</font><font color="#5fa233"><font size="6">Basic</font></font></h2>
+				  <div>
+					<div class="wsite-multicol">
+					  <div class="wsite-multicol-table-wrap" style="margin:0 -15px;">
+						<table class="wsite-multicol-table">
+						  <tbody class="wsite-multicol-tbody">
+							<tr class="wsite-multicol-tr">
+							  <td class="wsite-multicol-col" style="width:25.478260869565%; padding:0 15px;">
+								<div>
+								  <div class="wsite-image wsite-image-border-none " style="padding-top:10px;padding-bottom:10px;margin-left:0px;margin-right:0px;text-align:center">
+									<a><img src="/images/course/1/icon.png" alt="Picture" style="width:121;max-width:100%" /></a>
+								  <div style="display:block;font-size:90%"></div>
+								  </div>
+								</div>
+							  </td>
+						      <td class="wsite-multicol-col" style="width:74.521739130435%; padding:0 15px;">
+								<div class="paragraph"><font size="5" color="#2a2a2a">Learn to Create Account on Thunkable<br />Learn to Create your first App<br />Learn About Components</font><br /></div>
+							  </td>
+							</tr>
+						  </tbody>
+						</table>
+					  </div>
+					</div>
+				  </div>
+				  <div style="text-align:center;">
+					<div style="height: 10px; overflow: hidden;"></div>
+					<a class="wsite-button wsite-button-small wsite-button-highlight" href="/lesson/1/basic" ><span class="wsite-button-inner">Start Here</span></a>
+					<div style="height: 10px; overflow: hidden;"></div>
+				  </div>
+				  <div class="paragraph">&#8203;</div>
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </div>
+	  
+	</div>
+  </div>
+  
+  <a href="/team">Team Page</a>
+
+  </body>
+</html>
